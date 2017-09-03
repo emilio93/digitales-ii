eb3b2a24382391890af4a74e4a6f27fe721aef7b
Emilio Rojas <emilio93@gmail.com>
2017-09-03 03:31:03 -0600

    update registro4bits

--- tarea4/modulos/registro4bits.v
+++ tarea4/modulos/registro4bits.v
@@ -7,6 +7,9 @@
 `ifndef bitHolder
   `include "./bitHolder.v"
 `endif
+`ifndef serialOcontiguo
+  `include "./serialOcontiguo.v"
+`endif
 
 /*
   Modulo registro4bits
@@ -30,46 +33,64 @@ module registro4bits (
   output [3:0] q,     // estado del registro
   output s_out        // bit que sale cuando modo=00, es 0 para modo!=00
 );
+  parameter bits = 4;
+  wire [bits-1:0] s_out_bit;
+  wire socMSBout, socLSBout;
+  wire clkenb;
+
+  serialOcontiguo socMSB(
+    .usual(s_out_bit[0]),
+    .s_in(s_in),
+    .modo(modo),
+    .out(socMSBout)
+  );
+
+  serialOcontiguo socLSB(
+    .usual(s_out_bit[bits-1]),
+    .s_in(s_in),
+    .modo(modo),
+    .out(socLSBout)
+  );
 
   // El bitHolder 0 es el LSB
   bitHolder bitHolder0(
-    .s_der(s_der),
-    .s_izq(s_izq),
-    .d_n(d_n),
+    .s_der(socLSBout),
+    .s_izq(s_out_bit[1]),
+    .d_n(d[0]),
     .dir(dir),
     .modo(modo),
     .clkenb(clkenb),
-    .s_out(s_out)
+    .s_out(s_out_bit[0])
   );
 
   bitHolder bitHolder1(
-    .s_der(s_der),
-    .s_izq(s_izq),
-    .d_n(d_n),
+    .s_der(s_out_bit[0]),
+    .s_izq(s_out_bit[2]),
+    .d_n(d[0]),
     .dir(dir),
     .modo(modo),
     .clkenb(clkenb),
-    .s_out(s_out)
+    .s_out(s_out_bit[1])
   );
 
   bitHolder bitHolder2(
-    .s_der(s_der),
-    .s_izq(s_izq),
-    .d_n(d_n),
+    .s_der(s_out_bit[1]),
+    .s_izq(s_out_bit[3]),
+    .d_n(d[0]),
     .dir(dir),
     .modo(modo),
     .clkenb(clkenb),
-    .s_out(s_out)
+    .s_out(s_out_bit[2])
   );
 
   bitHolder bitHolder3(
-    .s_der(s_der),
-    .s_izq(s_izq),
-    .d_n(d_n),
+    .s_der(s_out_bit[1]),
+    .s_izq(socMSBout),
+    .d_n(d[0]),
     .dir(dir),
     .modo(modo),
     .clkenb(clkenb),
-    .s_out(s_out)
+    .s_out(s_out_bit[3])
   );
 
 
